933349cb926bff542dbc01757af08e229ee0226d
3asys <3asys@mail.ru>
2018-04-24 21:27:58 +0300

    2 итерация

--- hash1/index.js
+++ hash1/index.js
@@ -1,1 +1,4 @@
-console.log("Hello, World!");
+const crypto = require('crypto');
+var str = "BlockChain";
+var result = crypto.createHash("md5").update(str).digest("hex");
+console.log("str: " + str + ", result:" + result);
