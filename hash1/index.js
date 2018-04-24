e35b8e31ee493e71a85fbdc2de9a0cfe8b958c22
3asys <3asys@mail.ru>
2018-04-24 23:08:07 +0300

    5 итерация

--- hash1/index.js
+++ hash1/index.js
@@ -12,4 +12,20 @@ do {
 // get the public key in a compressed format
 const publicKey = secp256k1.publicKeyCreate(privateKey);
 console.log('1) Alice aquired new keypair:  publicKey: ' + publicKey.toString("hex") + ' privateKey: '+ privateKey.toString("hex"));
-function digest(str, algo = "sha256") {  return crypto.createHash(algo).update(str).digest();}
+
+// sign the message
+console.log('2) Alice signed her message digest with her privateKey to get its signature:');
+const sigObj = secp256k1.sign(digested, privateKey);
+const sig = sigObj.signature;
+console.log(" Signature:", sig.toString("hex"));
+
+// verification
+console.log('3) Bob verifyed by 3 elements ("message digest", "signature", and Alice\'s "publicKey"):');
+let verified = secp256k1.verify(digested, sig, publicKey);
+console.log(" verified:", verified);
+
+
+
+function digest(str, algo = "sha256") {  
+	return crypto.createHash(algo).update(str).digest();
+}
