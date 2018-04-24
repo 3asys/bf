c70838aa95b699cd7d3b629a4a964100e2e252c4
3asys <3asys@mail.ru>
2018-04-24 22:36:13 +0300

    3 итерация

--- hash1/index.js
+++ hash1/index.js
@@ -1,4 +1,10 @@
 const crypto = require('crypto');
-var str = "BlockChain";
-var result = crypto.createHash("md5").update(str).digest("hex");
-console.log("str: " + str + ", result:" + result);
+const secp256k1 = require('secp256k1');
+const msg = process.argv[2]; // message to be signed you pass
+const digested = digest(msg);
+console.log('0) Alice\'s message: message: '+ msg + ' message digest: '+ digested.toString("hex"));
+	
+function digest(str, algo = "sha256") {  
+	return crypto.createHash(algo).update(str).digest();
+}
+
