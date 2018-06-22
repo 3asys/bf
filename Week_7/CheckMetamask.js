e83c1fa62c153ef5b3765c2c6282198f6ee9a49f
3asys <3asys@mail.ru>
2018-06-23 01:37:16 +0300

    DApp 8

--- Week_7/CheckMetamask.js
+++ Week_7/CheckMetamask.js
@@ -3,13 +3,13 @@
 */
 window.addEventListener('load', function() {
 
-	///@dev Проверяем, что Metamask установлен:
+	//@dev Проверяем, что Metamask установлен:
 	if (typeof web3 !== 'undefined') { // Библиотека web3 определена - Metamask установлен
-		///@dev Проверяем, что Metamask разблокирован (массив счетов не пустой):
-		///@dev Используем асинхронный метод (web3.eth.getAccounts) вызова свойства "счета (accounts)"
-		///@dev https://github.com/MetaMask/metamask-extension/issues/1766
-		///@dev Это свойство только для чтения и возвращает список счетов
-		///@dev https://github.com/ethereum/wiki/wiki/JavaScript-API#web3ethaccounts
+		//@dev Проверяем, что Metamask разблокирован (массив счетов не пустой):
+		//@dev Используем асинхронный метод (web3.eth.getAccounts) вызова свойства "счета (accounts)"
+		//@dev https://github.com/MetaMask/metamask-extension/issues/1766
+		//@dev Это свойство только для чтения и возвращает список счетов
+		//@dev https://github.com/ethereum/wiki/wiki/JavaScript-API#web3ethaccounts
 		web3.eth.getAccounts(function (err, accounts) { 
 			if (accounts.length == 0) { // Массив счетов пустой - значит Metamask не разблокирован
 				alert('Unlock MetaMask to avoid errors while working with this application. Click on MetaMask icon in your browser extensions panel, enter your password and unlock your MetaMask wallet.');  
